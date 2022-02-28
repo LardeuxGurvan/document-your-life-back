b7505d32ff1ce69402c1e8a7de22245834d9ec4e
William Ng <william_ng94@hotmail.com>
2022-02-28 15:12:07 +0100

    mise en place de express-session

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
 const router = require('./routes');
 
 const app = express();
@@ -7,6 +8,24 @@ const app = express();
 app.use(express.json());
 
 app.use(cors(process.env.CORS_DOMAINS ?? '*'));
+
+const sess = {
+    secret: process.env.SESSION_PASSWORD,
+    cookie: {},
+};
+
+if (app.get('env') === 'production') {
+    app.set('trust proxy', 1); // trust first proxy
+    sess.cookie.secure = true; // serve secure cookies
+}
+
+app.use(session(sess));
+
+app.use((request, response, next) => {
+    response.locals.connectedUser = request.session.connectedUser;
+
+    next();
+});
 
 app.use(router);
 
