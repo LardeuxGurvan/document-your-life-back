07215caaea0a60b70e6501872aece1de9d1d9117
gurvan_lardeux <gurvan.lardeux@gmail.com>
2022-03-02 12:16:36 +0100

    try fixing cors with delete cookies functions, begin patch card

--- app/index.js
+++ app/index.js
@@ -1,34 +1,34 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
+// const bodyParser = require('body-parser');
+// const session = require('express-session');
+// const cookieParser = require('cookie-parser');
 const router = require('./routes');
 const app = express();
-app.use(cookieParser());
-app.use(bodyParser.json());
+// app.use(cookieParser());
+// app.use(bodyParser.json());
 app.use(express.json());
 
 app.use(cors(process.env.CORS_DOMAINS ?? '*'));
 
-const sess = {
-    secret: process.env.SESSION_PASSWORD,
-    cookie: {},
-};
+// const sess = {
+//     secret: process.env.SESSION_PASSWORD,
+//     cookie: {},
+// };
 
-if (app.get('env') === 'production') {
-    app.set('trust proxy', 1); // trust first proxy
-    sess.cookie.secure = true; // serve secure cookies
-}
+// if (app.get('env') === 'production') {
+//     app.set('trust proxy', 1); // trust first proxy
+//     sess.cookie.secure = true; // serve secure cookies
+// }
 
-app.use(session(sess));
+// app.use(session(sess));
 
-app.use((request, response, next) => {
-    console.log('local utilisateur : ', response.locals);
-    response.locals.connectedUser = request.session.connectedUser;
+// app.use((request, response, next) => {
+//     console.log('local utilisateur : ', response.locals);
+//     response.locals.connectedUser = request.session.connectedUser;
 
-    next();
-});
+//     next();
+// });
 
 
 app.use(router);
