d16a2c21863fd65911599c4b3a8bc63f9e035678
gurvan_lardeux <gurvan.lardeux@gmail.com>
2022-03-08 11:39:19 +0100

    upload on firebase, working on delete

--- app/controllers/card.js
+++ app/controllers/card.js
@@ -1,5 +1,6 @@
-const debug = require('debug')('app:cardController');
+// const debug = require('debug')('app:cardController');
 const fs = require('fs');
+const { getStorage, ref, deleteObject } = require('firebase/storage');
 const cardDataMapper = require('../models/card');
 const userDataMapper = require('../models/user');
 const { ApiError } = require('../helpers/errorHandler');
@@ -15,84 +16,6 @@ module.exports = {
         }
 
         return res.json(card);
-    },
-
-    // function create card
-    /**
-     * @param {String} text
-     * @param {String} video
-     * @param {String} audio
-     * @param {String} image
-     * @param {String} moodLabel
-     * @returns Created Card
-     */
-    async create(req, res) {
-        const { userId } = req.params;
-        const {
-            text,
-            video,
-            audio,
-            image,
-            moodLabel,
-        } = req.body;
-
-        // entire card cannot be empty
-        if (!text && !req.file && !moodLabel) {
-            throw new ApiError(403, 'At least one medium must be filled');
-        }
-
-        // find the last created card by user
-        const lastCard = await cardDataMapper.findLatestByUserPk(userId);
-
-        // Check if the request contains file
-        if (req.file) {
-            switch (req.file.fieldname) {
-            // add path to the body data
-            case 'image':
-                req.body.image = `${process.cwd()}/${req.file.path}`;
-                break;
-            case 'video':
-                req.body.video = `${process.cwd()}/${req.file.path}`;
-                break;
-            case 'audio':
-                req.body.audio = `${process.cwd()}/${req.file.path}`;
-                break;
-            default:
-                throw new ApiError(500, 'something went wrong');
-            }
-        }
-
-        // check if user created card before
-        if (!lastCard) {
-            const result = await cardDataMapper.create(
-                text,
-                video,
-                audio,
-                image,
-                moodLabel,
-                Number(userId),
-            );
-            return res.json(result);
-        }
-
-        // Compares current date with the created date of last card created
-        const lastCardDate = lastCard.created_at.toISOString().split('T')[0];
-        const currentDate = new Date().toISOString().split('T')[0];
-
-        if (lastCardDate === currentDate) {
-            throw new ApiError(403, 'card already created today');
-        }
-
-        // create card
-        const result = await cardDataMapper.create(
-            text,
-            video,
-            audio,
-            image,
-            moodLabel,
-            Number(userId),
-        );
-        return res.json(result);
     },
 
     async getAllElement(req, res) {
@@ -120,66 +43,6 @@ module.exports = {
         });
     },
 
-    async update(req, res) {
-        const { userId } = req.params;
-        const {
-            text,
-            moodLabel,
-        } = req.body;
-        console.log(req.files);
-
-        // At least one medium must be changed
-        if (!text && !req.files && !moodLabel) {
-            throw new ApiError(403, 'At least one medium must be changed');
-        }
-
-        // find the last created card by user
-        const lastCard = await cardDataMapper.findLatestByUserPk(userId);
-
-        // check if user created card before
-        if (!lastCard) {
-            throw new ApiError(404, 'There is no card created before');
-        }
-
-        // Compares current date with the created date of last card created
-        const lastCardDate = lastCard.created_at.toISOString().split('T')[0];
-        const currentDate = new Date().toISOString().split('T')[0];
-        debug('**********************************************************', lastCard);
-        if (lastCardDate !== currentDate) {
-            throw new ApiError(403, 'this is not the daily card');
-        }
-        // Check if the request contains file
-        if (req.files.image || req.files.video || req.files.audio) {
-            // add path to the body data
-            if (req.files.image) {
-                req.body.image = `${process.cwd()}/${req.files.image[0].path}`;
-                if (lastCard.image) {
-                    // if medium, delete this medium
-                    fs.unlinkSync(lastCard.image);
-                }
-            } else if (req.files.video) {
-                req.body.video = `${process.cwd()}/${req.files.video[0].path}`;
-                if (lastCard.video) {
-                    // if medium, delete this medium
-                    fs.unlinkSync(lastCard.video);
-                }
-            } else if (req.files.audio) {
-                req.body.audio = `${process.cwd()}/${req.files.audio[0].path}`;
-                if (lastCard.audio) {
-                    // if medium, delete this medium
-                    fs.unlinkSync(lastCard.audio);
-                }
-            } else {
-                throw new ApiError(500, 'something went wrong');
-            }
-        }
-        debug('files =>', req.files);
-
-        // update card
-        const savedResult = await cardDataMapper.update(lastCard.id, req.body);
-        return res.json(savedResult);
-    },
-
     async createOrUpdate(req, res) {
         const { userId } = req.params;
         const {
@@ -200,19 +63,32 @@ module.exports = {
             if (req.files.image || req.files.video || req.files.audio) {
                 // add path to the body data
                 if (req.files.image) {
-                    req.body.image = `${process.cwd()}/${req.files.image[0].path}`;
+                    req.body.image = req.files.image[0].firebaseUrl;
                     if (lastCard?.image) {
-                        // if medium, delete this medium
-                        fs.unlinkSync(lastCard.image);
+                        const storage = getStorage();
+                        const fileRef = storage(lastCard.image);
+
+                        console.log(`File in database before delete exists : ${
+                            fileRef.exists()}`);
+                        // Delete the file using the delete() method
+                        deleteObject(fileRef).then(() => {
+                            // File deleted successfully
+                            console.log('File Deleted');
+                        }).catch((error) => {
+                            // Some Error occurred
+                            throw new ApiError((500, `Error on delete: ${error.message}`));
+                        });
+                        console.log(`File in database after delete exists : ${
+                            fileRef.exists()}`);
                     }
                 } else if (req.files.video) {
-                    req.body.video = `${process.cwd()}/${req.files.video[0].path}`;
+                    req.body.video = req.files.video[0].firebaseUrl;
                     if (lastCard?.video) {
                         // if medium, delete this medium
                         fs.unlinkSync(lastCard.video);
                     }
                 } else if (req.files.audio) {
-                    req.body.audio = `${process.cwd()}/${req.files.audio[0].path}`;
+                    req.body.audio = req.files.audio[0].firebaseUrl;
                     if (lastCard?.audio) {
                         // if medium, delete this medium
                         fs.unlinkSync(lastCard.audio);
